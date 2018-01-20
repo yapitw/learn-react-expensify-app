83def4a14bb3e40a32ac30eedd343e49e88d06ea
yapi <yapi.tw@gmail.com>
2018-01-20 11:51:32 +0800

    Expensify app initiation

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
+import 'normalize.css/normalize.css'
+import './styles/style.scss';
+
+ReactDOM.render(<p>This is my boilerplate</p>, document.getElementById('app'));
