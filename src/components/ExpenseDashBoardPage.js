aa010af9001e8bf827c01299105d34ca31b064c1
yapi <yapi.tw@gmail.com>
2018-01-23 20:05:21 +0800

    Redux with React

--- src/components/ExpenseDashBoardPage.js
+++ src/components/ExpenseDashBoardPage.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import ExpenseList from './ExpenseList';
+import ExpenseListFilters from './ExpenseListFilters';
 
 const ExpenseDashBoardPage = () => (
   <div>
-    This is from dashboard component
+    <ExpenseListFilters />
+    <ExpenseList />
   </div>
 );
 
